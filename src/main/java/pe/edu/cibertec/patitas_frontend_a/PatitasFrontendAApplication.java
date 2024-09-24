2e78ea89871c1f2a8e5ed79f46ea072d31a21256
MasielZabaleta <masielzabaletalopez@gmail.com>
2024-09-24 15:27:30 -0500

    conectar backend con frontend

--- src/main/java/pe/edu/cibertec/patitas_frontend_a/PatitasFrontendAApplication.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/PatitasFrontendAApplication.java
@@ -2,6 +2,8 @@ package pe.edu.cibertec.patitas_frontend_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PatitasFrontendAApplication {
@@ -9,5 +11,8 @@ public class PatitasFrontendAApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PatitasFrontendAApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
@@ -1,20 +1,28 @@
 package pe.edu.cibertec.patitas_frontend_a.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginModel;
+import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginRequestDTO;
 
 @Controller
 @RequestMapping("/login")
+
 public class LoginController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String inicio(Model model) {
-        LoginModel loginModel = new LoginModel("00", "", "");
+        LoginModel loginModel = new LoginModel("00", "", "", "");
         model.addAttribute("loginModel", loginModel);
         return "inicio";
     }
@@ -24,24 +32,16 @@ public class LoginController {
                              @RequestParam("numeroDocumento") String numeroDocumento,
                              @RequestParam("password") String password,
                              Model model) {
+        LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
 
-        // Validar campos de entrada
-        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
-                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
-                password == null || password.trim().length() == 0) {
+        ResponseEntity<LoginModel> loginModelResponse = restTemplate.postForEntity("http://localhost:8080/autenticacion/login",loginRequestDTO, LoginModel.class);
 
-            LoginModel loginModel = new LoginModel("01", "Error: Debe completar correctamente sus credenciales", "");
-            model.addAttribute("loginModel", loginModel);
+        if (!loginModelResponse.getBody().codigo().equals("00")){
+            model.addAttribute("loginModel", loginModelResponse.getBody());
             return "inicio";
-
         }
 
-        // Invocar servicio de autenticación
-
-        LoginModel loginModel = new LoginModel("00", "", "César Santos");
-        model.addAttribute("loginModel", loginModel);
+        model.addAttribute("loginModel", loginModelResponse.getBody());
         return "principal";
-
     }
-
-}
+    }
